01681cf077274a3b3e7447deb44f6cdf5a49e52a
Justin Boltz <boltz.justin@gmail.com>
2016-07-18 19:51:59 -0500

    Add SPAM censorship

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    @posts.each do |post|
+      if post.id%5 == 0 || post.id == 1
+        post.update(title: "SPAM")
+      end
+    end
   end
 
   def show
