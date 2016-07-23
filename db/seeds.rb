b69bcb4298a46ee46f96d44ff5f3c6fd26997571
Justin Boltz <boltz.justin@gmail.com>
2016-07-23 16:51:46 -0500

    Add Sponsored Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,16 @@ require 'random_data'
 end
 topics = Topic.all
 
+20.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: RandomData.random_number
+  )
+end
+posts = Post.all  
+
 50.times do 
   Post.create!(
     topic: topics.sample,
@@ -24,7 +34,9 @@ posts = Post.all
   )
 end
 
+
 puts "Seed finished"
 puts "#{Topic.count} topics created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
